4883b657b32a5d890a45ce54ad862af8f518b7c1
Christopher Haynes <christopherhaynes33@gmail.com>
2024-02-18 20:37:59 -0500

    Cleaning branch after multiple merge conflicts

--- lib/Pages/browsing_page.dart
+++ lib/Pages/browsing_page.dart
@@ -3,6 +3,7 @@ import 'dart:convert';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import '../components/my_card.dart';
+import 'package:http/http.dart' as http;
 
 class BrowsingPage extends StatefulWidget {
   const BrowsingPage({super.key});
@@ -88,46 +89,5 @@ class _BrowsingPageState extends State<BrowsingPage> {
         ],
       ),
     ));
-  }
-}
-import 'package:flutter/material.dart';
-import 'package:http/http.dart' as http;
-
-class BrowsingPage extends StatefulWidget {
-  const BrowsingPage({super.key});
-
-  @override
-  State<BrowsingPage> createState() => _BrowsingPageState();
-}
-
-class _BrowsingPageState extends State<BrowsingPage> {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-          title: Text("browsing Stock Page"), automaticallyImplyLeading: false),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              "To other pages",
-            ),
-            ElevatedButton(
-              child: Text("Back to fav"),
-              onPressed: () {
-                Navigator.pop(context);
-              },
-            ),
-            ElevatedButton(
-              child: Text("to history"),
-              onPressed: () {
-                Navigator.of(context).pushNamed('history_page');
-              },
-            )
-          ],
-        ),
-      ),
-    );
   }
 }
