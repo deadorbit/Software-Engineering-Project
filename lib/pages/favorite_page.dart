08f3489bc9887ee4f7e75b46b633ee2d670d1106
deadorbit <65923075+deadorbit@users.noreply.github.com>
2024-02-20 17:16:15 -0500

    Updated to Dev
    Also added some graphical changes to authentication

---
+++ lib/pages/favorite_page.dart
@@ -0,0 +1,65 @@
+import 'package:flutter/material.dart';
+import '../service/controller.dart';
+import "package:firebase_auth/firebase_auth.dart";
+
+class FavoritePage extends StatefulWidget {
+  const FavoritePage({super.key});
+
+  @override
+  State<FavoritePage> createState() => _FavoritePageState();
+}
+
+class _FavoritePageState extends State<FavoritePage> {
+  final user = FirebaseAuth.instance.currentUser!;
+
+  void signUserOut(BuildContext context) {
+    FirebaseAuth.instance.signOut();
+    Navigator.pushReplacementNamed(context, '/landing');
+  }
+
+  final databaseController = DataBase_Controller();
+  String result = "Loading..."; // Initial state of the result
+  void getUsers() async {
+    String usersResult = await databaseController.getUsers();
+    setState(() {
+      result = usersResult; // Update the result with fetched data
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        actions: [
+          IconButton(
+            onPressed: () => signUserOut(context), // Pass context here
+            icon: const Icon(Icons.logout),
+          ),
+        ],
+        title: Center(child: Text("Saved Quotes", textAlign: TextAlign.center)),
+      ),
+      body: Container(
+        decoration: BoxDecoration(
+            image: DecorationImage(
+          image: AssetImage("lib/images/background.png"),
+          fit: BoxFit.cover,
+        )),
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              ElevatedButton(
+                  child: Text("data"),
+                  onPressed: () {
+                    getUsers();
+                  }),
+              SizedBox(
+                height: 25,
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/service/controller.dart
@@ -0,0 +1,19 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+
+class DataBase_Controller {
+  DataBase_Controller();
+
+  Future<String> getUsers() async {
+    try {
+      QuerySnapshot querySnapshot =
+          await FirebaseFirestore.instance.collection('fav_stocks').get();
+      for (var doc in querySnapshot.docs) {
+        print('Name: ${doc['name']}, Price: ${doc["price"]}');
+      }
+      return "done";
+    } catch (e) {
+      print(e);
+      return "error";
+    }
+  }
+}
